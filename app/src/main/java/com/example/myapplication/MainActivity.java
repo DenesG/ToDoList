c9f01cf4baf684240cc411a6f6dd9e9641dab3ad
denes <denesgyorgy97@gmail.com>
2021-11-16 19:46:41 -0800

    did some stuffs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.myapplication.Adapter.ToDoAdapter;
+import com.example.myapplication.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private List<ToDoModel> taskList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        RecyclerView tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ToDoAdapter taskAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(taskAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("YASSSSSSIFICATION OF ANDROID STUDIO");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        taskAdapter.setTasks(taskList);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Utils/DatabaseHandler.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.Utils;
+
+public class DatabaseHandler {
+
+
+}
